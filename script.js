b0e01e74e7285b4769641c965c9cebf2b68f720e
cinekfarmer <cinekfarmer@gmail.com>
2026-06-05 22:04:31 +0200

    Add script to switch languages and basic language JSON files

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const USER_INPUT = document.querySelector("#userInput");
-const CONSOLE_OUTPUT = document.querySelector("#consoleOutput")
+const TRANSLATIONABLE_ELEMENTS_LIST = document.querySelectorAll("[data-i18n]");
+console.log(TRANSLATIONABLE_ELEMENTS_LIST.length);
+const LANG_LIST = ['en', 'pl', 'sv'];
+let translations;
 
-document.addEventListener("keydown", (e) => {
-if (e.key === "Enter") updateConsole(USER_INPUT.value);
+LANG_LIST.forEach(language => {
+  document.querySelector('#' + language + 'Btn').addEventListener('click', async () => {
+    console.log(language);
+    translations = await loadLanguage(language);
+    console.log(translations.header.subtitle);
+    switchText(translations);
+  });
 });
 
-function updateConsole(userInput) {
-  const newUserInput = `<p>${userInput}</p>`;
-  USER_INPUT.value = "";
-  CONSOLE_OUTPUT.innerHTML = CONSOLE_OUTPUT.innerHTML + newUserInput;
+async function loadLanguage(language) {
+  let response = await fetch(`translations/${language}/main.json`);
+
+  if (!response.ok) throw new Error(`Can't load language ${language}`);
+  let translations = await response.json();
+  return translations;
+}
+
+function getNestedValue(obj, path) {
+  return path.split(".").reduce((previous, current) => {
+    return previous ? previous[current] : undefined;
+  }, obj);
+}
+
+function switchText(translations) {
+  console.log(translations);
+  TRANSLATIONABLE_ELEMENTS_LIST.forEach((element) => {
+    let elementKey = element.getAttribute("data-i18n");
+    let text = getNestedValue(translations, elementKey);
+
+    if(text) element.textContent = text;
+    else console.log(`Couldn't find ${elementKey}`);
+  })
+  console.log("switching text complete")
 }
